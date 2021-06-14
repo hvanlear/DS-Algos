623fba8282b8eae0c3a561b8d83451701c0e7ae4
Hunter <hvanlear@gmail.com>
2021-06-14 11:15:37 -0400

    binary search tree

---
+++ binarySearchTrees/BSTexercise.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinarySearchTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** insert(val): insert a new node into the BST with value val.
+   * Returns the tree. Uses iteration. */
+
+  insert(val) {
+    // if the tree is empty insert at the root
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
+
+    // Otherwise find the corret spot for the new node.
+
+    let current = this.root;
+
+    while (true) {
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = new Node(val);
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else if (val > current.val) {
+        if (current.right === null) {
+          current.right = new Node(val);
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+  }
+}
